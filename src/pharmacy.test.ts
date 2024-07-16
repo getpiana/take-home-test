0f68851c020020b4d7b9b6420d69ba5b72205e40
Clément <clement2104.boillot@gmail.com>
2024-07-16 19:21:21 +0200

    feat: added Dafalgan drug
    - Added tests for normal & expired behavior
    - Added checks in Pharmacy

--- src/pharmacy.test.ts
+++ src/pharmacy.test.ts
@@ -6,4 +6,22 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)],
     );
   });
+  it("normal Dafalgan should decrease the benefit twice as fast as Doliprane", () => {
+    // Normal behavior
+    const drugs = [new Drug("Doliprane", 2, 3), new Drug("Dafalgan", 2, 3)];
+    const pharmacy = new Pharmacy(drugs);
+    expect(pharmacy.updateBenefitValue()).toEqual([
+      new Drug("Doliprane", 1, 2),
+      new Drug("Dafalgan", 1, 1),
+    ]);
+  });
+  it("expired Dafalgan should decrease the benefit twice as fast as Doliprane", () => {
+    // Expired behavior
+    const drugs = [new Drug("Doliprane", 0, 5), new Drug("Dafalgan", 0, 5)];
+    const pharmacy = new Pharmacy(drugs);
+    expect(pharmacy.updateBenefitValue()).toEqual([
+      new Drug("Doliprane", -1, 3),
+      new Drug("Dafalgan", -1, 1),
+    ]);
+  });
 });
--- src/pharmacy.ts
+++ src/pharmacy.ts
@@ -20,7 +20,11 @@ export class Pharmacy {
         this.drugs[i].name != "Herbal Tea" &&
         this.drugs[i].name != "Fervex"
       ) {
-        if (this.drugs[i].benefit > 0) {
+        if (this.drugs[i].name == "Dafalgan") {
+          if (this.drugs[i].benefit > 1) {
+            this.drugs[i].benefit -= 2;
+          }
+        } else if (this.drugs[i].benefit > 0) {
           if (this.drugs[i].name != "Magic Pill") {
             this.drugs[i].benefit = this.drugs[i].benefit - 1;
           }
@@ -48,7 +52,11 @@ export class Pharmacy {
       if (this.drugs[i].expiresIn < 0) {
         if (this.drugs[i].name != "Herbal Tea") {
           if (this.drugs[i].name != "Fervex") {
-            if (this.drugs[i].benefit > 0) {
+            if (this.drugs[i].name == "Dafalgan") {
+              if (this.drugs[i].benefit > 1) {
+                this.drugs[i].benefit = this.drugs[i].benefit - 2;
+              }
+            } else if (this.drugs[i].benefit > 0) {
               if (this.drugs[i].name != "Magic Pill") {
                 this.drugs[i].benefit = this.drugs[i].benefit - 1;
               }
